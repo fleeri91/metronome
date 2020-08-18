c460c4a7bc689b5e90773214ac5f598f9c7f8a9e
fleeri91 <fleeri91@gmail.com>
2020-08-18 18:17:34 +0200

    Updates

---
+++ precache-manifest.7c553e448961ce60dab5407a0e32a0fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de904f827c2395836b8f1bee41f07e78",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "b5e143f0ac3e01185ba9",
+    "url": "/metronome/static/css/main.dfd4b263.chunk.css"
+  },
+  {
+    "revision": "44e429873ebf11c93a12",
+    "url": "/metronome/static/js/2.3a21db6c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/metronome/static/js/2.3a21db6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e143f0ac3e01185ba9",
+    "url": "/metronome/static/js/main.307df5e9.chunk.js"
+  },
+  {
+    "revision": "f8600e950f693645d24a",
+    "url": "/metronome/static/js/runtime-main.e61688e8.js"
+  },
+  {
+    "revision": "7500ba6a251e54e014fec6bfa4270a8b",
+    "url": "/metronome/static/media/bpm-klack.7500ba6a.wav"
+  },
+  {
+    "revision": "f7e089e41704e4292191c8207a24f010",
+    "url": "/metronome/static/media/bpm-pulse.f7e089e4.wav"
+  }
+]);
